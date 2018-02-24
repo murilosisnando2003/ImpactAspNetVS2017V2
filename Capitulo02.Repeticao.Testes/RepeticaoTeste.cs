328c681abe37b627595ec0af562147029d8cbe3f
Murilo Rodrigues <murilosisnando2003@hotmail.com>
2018-02-24 16:53:54 -0300

    go
    go

---
+++ Capitulo02.Repeticao.Testes/RepeticaoTeste.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capitulo02.Repeticao.Testes
+{
+    [TestClass]
+    public class RepeticaoTeste
+    {
+        [TestMethod]
+        public void ForAninhadoTeste()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i*j}");
+                }
+                Console.WriteLine(new string('-',50));
+            }
+        }
+        [TestMethod]
+        public void EstruturaForTeste()
+        {
+            int i = 1;
+
+            for (Console.WriteLine("iniciou"); i < 3; Console.WriteLine(i))
+            {
+                i++;
+            }
+
+        }
+    }
+}
