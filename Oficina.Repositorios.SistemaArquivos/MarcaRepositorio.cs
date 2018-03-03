55bf53d9701b798b740a941b2dc90bcb74d66bfc
Murilo Rodrigues <murilosisnando2003@hotmail.com>
2018-03-03 13:27:39 -0300

    1

---
+++ Oficina.Repositorios.SistemaArquivos/MarcaRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Oficina.Dominio;
+
+namespace Oficina.Repositorios.SistemaArquivos
+{
+    public class CorRepositorio
+    {
+        public List<Marca> Selecionar()
+        {
+            var marcas = new List<Marca>();
+
+            foreach (var linha in File.ReadAllLines(@"Dados\Marca.txt"))
+            {
+                var marca = new Marca();
+                marca.Id = Convert.ToInt32(linha.Substring(0, 5));
+                marca.Nome = linha.Substring(5);
+
+                marcas.Add(marca);
+
+            }
+
+            return marcas;
+
+        }
+    }
+}
